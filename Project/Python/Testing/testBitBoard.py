41a3335c76eb22a2028e39a894f784828669c929
Nguyen Cong Minh <nguyencongminh050@gmail.com>
2024-11-11 22:01:37 +0700

    Update testBitBoard.py

--- Project/Python/Testing/testBitBoard.py
+++ Project/Python/Testing/testBitBoard.py
@@ -35,12 +35,12 @@ class BitBoard:
         Purpose: Generate Hash value for a position.
         """
         hashValue = 0
-        for row in range(self.size):
-            for col in range(self.size):
+        for row in range(self.__size):
+            for col in range(self.__size):
                 state = self.getState((row, col))
                 if state:
                     hashValue ^= self.__zobristTable[(row, col, state)]
-        return hashValue
+        return hex(hashValue)[2:].upper()
 
     def addMove(self, move: tuple[int, int], player):
         """
@@ -49,10 +49,45 @@ class BitBoard:
         """
         if self.getState(move) is not None:
             return 
-        if player == 1:
-            pos = move[0] * self.__size + move[1]
-            self.bitBoard = 0b01 << (pos * 2)
+        pos = move[0] * self.__size + move[1]
+        self.bitBoard |= player << (pos * 2)
+
+    def view(self):
+        line = []
+        for x in range(self.__size):
+            curLine = []
+            for y in range(self.__size):
+                state = self.getState((x, y))
+                if state:
+                    curLine.append('X' if state == 1 else "O")
+                else:
+                    curLine.append('.')
+            line.append('  '.join(curLine))
+        return '\n'.join(line)
+    
+    def is_win(self, player: int) -> bool:
+        directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
+        for row in range(self.size):
+            for col in range(self.size):
+                if self.getState((row, col)) == player:
+                    for dRow, dCol in directions:
+                        count = 1
+                        r, c = row + dRow, col + dCol
+                        while 0 <= r < self.size and 0 <= c < self.size and self.get_state((r, c)) == player:
+                            count += 1
+                            r += dRow
+                            c += dCol
+                        if count >= 5:
+                            print(f"Player '{player}' has won!")
+                            return True
+        print(f"Player '{player}' has not won.")
+        return False
 
     
 
 bitBoard = BitBoard(15)
+bitBoard.addMove((7, 12), 1)
+print(bitBoard.hash())
+bitBoard.addMove((8, 12), 1)
+print(bitBoard.hash())
+print(bitBoard.view())
